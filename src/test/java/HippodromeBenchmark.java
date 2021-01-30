34ecd42d8bec81b258a669c0b50fc9d2accaeb9a
Sigmund <sigmundchianasta@gmail.com>
2021-01-30 22:09:47 +0800

    Update HippodromeBenchmark.java to use hippodrome

--- src/test/java/HippodromeBenchmark.java
+++ src/test/java/HippodromeBenchmark.java
@@ -10,9 +10,9 @@ public class HippodromeBenchmark {
     @Test
     public void airlineBenchmark() throws IOException, InterruptedException {
         Runtime rt = Runtime.getRuntime();
-        Process pr = rt.exec("java -jar /RacerDFix-1.0-jar-with-dependencies.jar " +
+        Process pr = rt.exec("java -jar /hippodrome-1.0-jar-with-dependencies.jar " +
                 "--config_file=src/test/java/airline/CONFIG.json");
-        int returnValue = pr.waitFor(); // RacerDFix returns 0 when no more bugs are detected.
+        int returnValue = pr.waitFor(); // hippodrome returns 0 when no more bugs are detected.
 
         assertEquals(SUCCESS_VALUE, returnValue);
     }
@@ -20,9 +20,9 @@ public class HippodromeBenchmark {
     @Test
     public void alarmclockBenchmark() throws IOException, InterruptedException {
         Runtime rt = Runtime.getRuntime();
-        Process pr = rt.exec("java -jar /RacerDFix-1.0-jar-with-dependencies.jar " +
+        Process pr = rt.exec("java -jar /hippodrome-1.0-jar-with-dependencies.jar " +
                 "--config_file=src/test/java/alarmclock/CONFIG.json");
-        int returnValue = pr.waitFor(); // RacerDFix returns 0 when no more bugs are detected.
+        int returnValue = pr.waitFor(); // hippodrome returns 0 when no more bugs are detected.
 
         assertEquals(SUCCESS_VALUE, returnValue);
     }
@@ -30,9 +30,9 @@ public class HippodromeBenchmark {
     @Test
     public void buggyprogramBenchmark() throws IOException, InterruptedException {
         Runtime rt = Runtime.getRuntime();
-        Process pr = rt.exec("java -jar /RacerDFix-1.0-jar-with-dependencies.jar " +
+        Process pr = rt.exec("java -jar /hippodrome-1.0-jar-with-dependencies.jar " +
                 "--config_file=src/test/java/buggyprogram/CONFIG.json");
-        int returnValue = pr.waitFor(); // RacerDFix returns 0 when no more bugs are detected.
+        int returnValue = pr.waitFor(); // hippodrome returns 0 when no more bugs are detected.
 
         assertEquals(SUCCESS_VALUE, returnValue);
     }
@@ -40,9 +40,9 @@ public class HippodromeBenchmark {
     @Test
     public void consisitencyBenchmark() throws IOException, InterruptedException {
         Runtime rt = Runtime.getRuntime();
-        Process pr = rt.exec("java -jar /RacerDFix-1.0-jar-with-dependencies.jar " +
+        Process pr = rt.exec("java -jar /hippodrome-1.0-jar-with-dependencies.jar " +
                 "--config_file=src/test/java/consisitency/CONFIG.json");
-        int returnValue = pr.waitFor(); // RacerDFix returns 0 when no more bugs are detected.
+        int returnValue = pr.waitFor(); // hippodrome returns 0 when no more bugs are detected.
 
         assertEquals(SUCCESS_VALUE, returnValue);
     }
@@ -50,9 +50,9 @@ public class HippodromeBenchmark {
     @Test
     public void dataraceBenchmark() throws IOException, InterruptedException {
         Runtime rt = Runtime.getRuntime();
-        Process pr = rt.exec("java -jar /RacerDFix-1.0-jar-with-dependencies.jar " +
+        Process pr = rt.exec("java -jar /hippodrome-1.0-jar-with-dependencies.jar " +
                 "--config_file=src/test/java/datarace/CONFIG.json");
-        int returnValue = pr.waitFor(); // RacerDFix returns 0 when no more bugs are detected.
+        int returnValue = pr.waitFor(); // hippodrome returns 0 when no more bugs are detected.
 
         assertEquals(SUCCESS_VALUE, returnValue);
     }
@@ -60,9 +60,9 @@ public class HippodromeBenchmark {
     @Test
     public void linkedlistBenchmark() throws IOException, InterruptedException {
         Runtime rt = Runtime.getRuntime();
-        Process pr = rt.exec("java -jar /RacerDFix-1.0-jar-with-dependencies.jar " +
+        Process pr = rt.exec("java -jar /hippodrome-1.0-jar-with-dependencies.jar " +
                 "--config_file=src/test/java/linkedlist/CONFIG.json");
-        int returnValue = pr.waitFor(); // RacerDFix returns 0 when no more bugs are detected.
+        int returnValue = pr.waitFor(); // hippodrome returns 0 when no more bugs are detected.
 
         assertEquals(SUCCESS_VALUE, returnValue);
     }
@@ -70,9 +70,9 @@ public class HippodromeBenchmark {
     @Test
     public void mergesortBenchmark() throws IOException, InterruptedException {
         Runtime rt = Runtime.getRuntime();
-        Process pr = rt.exec("java -jar /RacerDFix-1.0-jar-with-dependencies.jar " +
+        Process pr = rt.exec("java -jar /hippodrome-1.0-jar-with-dependencies.jar " +
                 "--config_file=src/test/java/mergesort/CONFIG.json");
-        int returnValue = pr.waitFor(); // RacerDFix returns 0 when no more bugs are detected.
+        int returnValue = pr.waitFor(); // hippodrome returns 0 when no more bugs are detected.
 
         assertEquals(SUCCESS_VALUE, returnValue);
     }
@@ -80,9 +80,9 @@ public class HippodromeBenchmark {
     @Test
     public void nestedclasses1Benchmark() throws IOException, InterruptedException {
         Runtime rt = Runtime.getRuntime();
-        Process pr = rt.exec("java -jar /RacerDFix-1.0-jar-with-dependencies.jar " +
+        Process pr = rt.exec("java -jar /hippodrome-1.0-jar-with-dependencies.jar " +
                 "--config_file=src/test/java/nestedclasses1/CONFIG.json");
-        int returnValue = pr.waitFor(); // RacerDFix returns 0 when no more bugs are detected.
+        int returnValue = pr.waitFor(); // hippodrome returns 0 when no more bugs are detected.
 
         assertEquals(SUCCESS_VALUE, returnValue);
     }
@@ -90,9 +90,9 @@ public class HippodromeBenchmark {
     @Test
     public void nestedclasses2Benchmark() throws IOException, InterruptedException {
         Runtime rt = Runtime.getRuntime();
-        Process pr = rt.exec("java -jar /RacerDFix-1.0-jar-with-dependencies.jar " +
+        Process pr = rt.exec("java -jar /hippodrome-1.0-jar-with-dependencies.jar " +
                 "--config_file=src/test/java/nestedclasses2/CONFIG.json");
-        int returnValue = pr.waitFor(); // RacerDFix returns 0 when no more bugs are detected.
+        int returnValue = pr.waitFor(); // hippodrome returns 0 when no more bugs are detected.
 
         assertEquals(SUCCESS_VALUE, returnValue);
     }
@@ -100,9 +100,9 @@ public class HippodromeBenchmark {
     @Test
     public void nestedclasses3Benchmark() throws IOException, InterruptedException {
         Runtime rt = Runtime.getRuntime();
-        Process pr = rt.exec("java -jar /RacerDFix-1.0-jar-with-dependencies.jar " +
+        Process pr = rt.exec("java -jar /hippodrome-1.0-jar-with-dependencies.jar " +
                 "--config_file=src/test/java/nestedclasses3/CONFIG.json");
-        int returnValue = pr.waitFor(); // RacerDFix returns 0 when no more bugs are detected.
+        int returnValue = pr.waitFor(); // hippodrome returns 0 when no more bugs are detected.
 
         assertEquals(SUCCESS_VALUE, returnValue);
     }
@@ -110,9 +110,9 @@ public class HippodromeBenchmark {
     @Test
     public void pingpongBenchmark() throws IOException, InterruptedException {
         Runtime rt = Runtime.getRuntime();
-        Process pr = rt.exec("java -jar /RacerDFix-1.0-jar-with-dependencies.jar " +
+        Process pr = rt.exec("java -jar /hippodrome-1.0-jar-with-dependencies.jar " +
                 "--config_file=src/test/java/pingpong/CONFIG.json");
-        int returnValue = pr.waitFor(); // RacerDFix returns 0 when no more bugs are detected.
+        int returnValue = pr.waitFor(); // hippodrome returns 0 when no more bugs are detected.
 
         assertEquals(SUCCESS_VALUE, returnValue);
     }
@@ -120,9 +120,9 @@ public class HippodromeBenchmark {
     @Test
     public void wronglock2Benchmark() throws IOException, InterruptedException {
         Runtime rt = Runtime.getRuntime();
-        Process pr = rt.exec("java -jar /RacerDFix-1.0-jar-with-dependencies.jar " +
+        Process pr = rt.exec("java -jar /hippodrome-1.0-jar-with-dependencies.jar " +
                 "--config_file=src/test/java/wronglock2/CONFIG.json");
-        int returnValue = pr.waitFor(); // RacerDFix returns 0 when no more bugs are detected.
+        int returnValue = pr.waitFor(); // hippodrome returns 0 when no more bugs are detected.
 
         assertEquals(SUCCESS_VALUE, returnValue);
     }
